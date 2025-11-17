d71c9628ac46a03c6c4b9538dd48ca879a44cc41
Mehdev9 <mehdi-belkateb@hotmail.com>
2025-11-17 14:15:20 +0100

    creation vue, layout, route

---
+++ resources/views/components/nav.blade.php
@@ -0,0 +1,8 @@
+<nav>
+    <ul>
+        <li><a href="{{ url('/') }}">Accueil</a></li>
+        <li><a href="{{ route('profil') }}">Profil</a></li>
+        <li><a href="{{ route('product') }}">Produits</a></li>
+        <li><a href="{{ route('detailProduct') }}">Détail</a></li>
+    </ul>
+</nav>
